717bcf7113034cb3f1ed898158fae6789a16214a
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-15 19:13:37 +0530

    remove png style note and improvements in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 
 const App = () => {
+  const formHandler = (e) => {
+    console.log(`form submited`)
+    e.preventDefault();
+  }
   return (
-    <div className=' h-screen bg-black  text-white  '>
-      <form action="" className='flex p-10  items-start justify-between'>
-        <div className='flex-col w-1/2 items-start flex gap-5' >
-          <input type="text" 
-        placeholder='Enter Task Heading'
-        className=' px-5 py-2 border-2 rounded w-full'
-         />
+    <div className=' h-screen bg-black  text-white p-10 '>
+      <form onSubmit={(e) => {
+        formHandler(e)
+      }} className='flex items-start justify-between flex-col'>
 
-         <input type="text" 
-        placeholder='Enter Details '
-        className=' px-5 py-2 border-2 rounded h-20 w-full'
-         />
+        <input type="text"
+          placeholder='Enter Task Heading'
+          className=' px-5 py-2 border-2 rounded w-full outline-none font-medium'
+        />
 
-        <button className=' bg-white text-black px-5 py-2 rounded w-full'>Add Note</button>
-        </div>
+        <textarea type="text"
+          placeholder='Enter Details '
+          className=' px-5 py-2 border-2 rounded h-32 w-full outline-none flex flex-row font-medium'
+        />
+
+        <button className=' bg-white text-black px-5 py-2 rounded w-full outline-none font-medium'>Add Note</button>
       </form>
+
+      <div>
+        <div className='h-32 w-32 bg-white rounded-2xl'>
+
+        </div>
+      </div>
     </div>
   )
 }
