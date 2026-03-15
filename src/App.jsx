689f5e5449cd6a8014f22b340b155c2509978731
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-15 18:36:51 +0530

    make correct arrgenments for buttons and inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 const App = () => {
   return (
     <div className=' h-screen bg-black  text-white  '>
-      <form action="" className='flex p-10 flex-col gap-5 items-start'>
-        <input type="text" 
+      <form action="" className='flex p-10  items-start justify-between'>
+        <div className='flex-col w-1/2 items-start flex gap-5' >
+          <input type="text" 
         placeholder='Enter Task Heading'
-        className=' px-5 py-2 border-2 rounded w-1/2 '
+        className=' px-5 py-2 border-2 rounded w-full'
          />
 
          <input type="text" 
         placeholder='Enter Details '
-        className=' px-5 py-2 border-2 rounded h-20 w-1/2 '
+        className=' px-5 py-2 border-2 rounded h-20 w-full'
          />
 
-        <button className=' bg-white text-black px-5 py-2 rounded w-1/2'>Add Note</button>
+        <button className=' bg-white text-black px-5 py-2 rounded w-full'>Add Note</button>
+        </div>
       </form>
     </div>
   )
