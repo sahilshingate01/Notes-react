de6550d2b18ba048d8491ab01fcc242a7321e92e
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-19 20:47:25 +0530

    add functionality for delete button

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,11 @@ const App = () => {
     settitle('')
     setdetail('')
   }
-
+  function deleteNote(idx){
+    const copytask = [...task]
+    copytask.splice(idx,1)
+    settask(copytask)
+  }
   return (
     <div className=' h-screen bg-black lg:flex  text-white p-10 '>
 
@@ -59,7 +63,9 @@ const App = () => {
             return <div key={idx} className=' flex justify-between flex-col items-start h-52 w-40 relative rounded-2xl p-2 bg-[url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7qMZEJSG6vy2aOHZ7uuSF75vX7cOAgTl3sQ&s")] bg-cover'>
               <h2 className='text-3xl text-black font-bold mt-2'> {ele.title} </h2>
               <p className='text-xl text-gray-700 mb-16'>{ele.detail}</p>
-              <button className='bg-red-500 w-full text-white font-bold rounded active:scale-95 cursor-pointer '>Delete</button>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} className='bg-red-500 w-full text-white font-bold rounded active:scale-95 cursor-pointer '>Delete</button>
             </div>
           })}
         </div>
