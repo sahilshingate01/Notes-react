528beb810e04c915eeb1021235f0bcd84dbacaea
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-19 19:40:55 +0530

    notes ui added and functionality also added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [title, settitle] = useState('')
+  const [detail, setdetail] = useState('')
+  const [task, settask] = useState([])
+
   const formHandler = (e) => {
-    console.log(`form submited`)
-    e.preventDefault();
+     e.preventDefault()
+
+    const copytask = [...task];
+
+    copytask.push({ title, detail })
+
+    settask(copytask)
+    console.log(copytask)
+    settitle('')
+    setdetail('')
   }
+
   return (
-    <div className=' h-screen bg-black  text-white p-10 '>
+    <div className=' h-screen bg-black lg:flex  text-white p-10 '>
+
       <form onSubmit={(e) => {
         formHandler(e)
-      }} className='flex items-start justify-between flex-col'>
+      }} className='flex items-start flex-col p-10 gap-4 lg:w-1/2'>
+
+        <h1 className=' text-5xl font-bold'>Add Notes</h1>
+
+        {/* title input */}
 
         <input type="text"
           placeholder='Enter Task Heading'
           className=' px-5 py-2 border-2 rounded w-full outline-none font-medium'
+          value={title}
+          onChange={(e) => settitle(e.target.value)}
         />
+
+        {/* short notes for heading */}
 
         <textarea type="text"
           placeholder='Enter Details '
           className=' px-5 py-2 border-2 rounded h-32 w-full outline-none flex flex-row font-medium'
+          value={detail}
+          onChange={(e) => { setdetail(e.target.value) }}
         />
 
-        <button className=' bg-white text-black px-5 py-2 rounded w-full outline-none font-medium'>Add Note</button>
+        <button className=' bg-white text-black px-5 py-2 rounded w-full outline-none font-medium active:scale-95'>Add Note</button>
+
       </form>
 
-      <div>
-        <div className='h-32 w-32 bg-white rounded-2xl'>
+      <div className=' lg:border-l-2 p-10 lg:w-1/2 '>
 
+        <h1 className=' text-5xl font-bold'>Your Notes</h1>
+
+        <div className='flex flex-wrap gap-5 mt-5 overflow-auto h-full'>
+          {task.map((ele,idx) => {
+            return <div key={idx} className='h-52 w-40 bg-white rounded-2xl'></div>
+          })}
         </div>
+
       </div>
     </div>
   )
